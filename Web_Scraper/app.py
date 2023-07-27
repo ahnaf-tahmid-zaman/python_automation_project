405bb6dbde4e2f1e9610cf466eed2a412334580b
AHNAF14924 <ahnaftahmid14924@gmail.com>
2023-07-28 00:03:41 +0600

    Added code

--- Web_Scraper/app.py
+++ Web_Scraper/app.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import openpyxl
+
+excel = openpyxl.Workbook()
+sheet = excel.active
+sheet.title = "price"
+sheet.append(['Name', 'Price'])
 try:
     url = requests.get(
         'https://www.techlandbd.com/pc-components/graphics-card?limit=100')
@@ -11,5 +17,7 @@ try:
         title = card.find('div', class_='name').a.text
         price = card.find('div', class_='price').span.text
         print(title, price)
+        sheet.append([title, price])
 except Exception as e:
     print(e)
+excel.save('Graphics Card.xlsx')
