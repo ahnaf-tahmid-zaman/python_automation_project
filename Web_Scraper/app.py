f574b18c649e15f638312b59445b84d1683588c0
AHNAF14924 <ahnaftahmid14924@gmail.com>
2023-07-28 01:15:18 +0600

    Added code

--- Web_Scraper/app.py
+++ Web_Scraper/app.py
@@ -2,24 +2,53 @@ from bs4 import BeautifulSoup
 import requests
 import openpyxl
 
-excel = openpyxl.Workbook()
-sheet = excel.active
-sheet.title = "price"
-sheet.append(['Brand', 'Name', 'Price'])
-try:
-    url = requests.get(
-        'https://www.techlandbd.com/pc-components/graphics-card?sort=p.price&order=ASC&fq=1&limit=100')
-    url.raise_for_status()
-    soup = BeautifulSoup(url.text, 'html.parser')
-    cards = soup.find(
-        'div', class_='main-products product-grid').find_all('div', class_='product-layout has-extra-button')
-    for card in cards:
-        name = card.find('div', class_='name').a.text
-        title = name.split(' ', 1)[1]
-        brand = name.split(' ', 1)[0]
-        price = card.find('div', class_='price').span.text
-        print(brand, title, price)
-        sheet.append([brand, title, price])
-except Exception as e:
-    print(e)
-excel.save('Graphics Card.xlsx')
+
+def extract_brand_name_and_title(name):
+    # Split the name and return the first word as the brand name and the rest as title
+    brand, title = name.split(' ', 1)
+    return brand, title
+
+
+def scrape_graphics_cards_data():
+    try:
+        # Create a new Excel workbook and set up the worksheet
+        excel = openpyxl.Workbook()
+        sheet = excel.active
+        sheet.title = "price"
+        sheet.append(['Brand', 'Name', 'Price'])
+
+        url = 'https://www.techlandbd.com/pc-components/graphics-card?sort=p.price&order=ASC&fq=1&limit=100'
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find all product cards on the webpage
+        cards = soup.find('div', class_='main-products product-grid').find_all(
+            'div', class_='product-layout has-extra-button')
+
+        for card in cards:
+            # Extract the product name
+            name = card.find('div', class_='name').a.text
+
+            # Split the name to get the brand and title
+            brand, title = extract_brand_name_and_title(name)
+
+            # Extract the product price
+            price = card.find('div', class_='price').span.text
+
+            # Print the product details and add them to the Excel sheet
+            print(brand, title, price)
+            sheet.append([brand, title, price])
+
+        # Save the Excel file
+        excel.save('Graphics Card.xlsx')
+
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+if __name__ == "__main__":
+    # Call the main scraping function
+    scrape_graphics_cards_data()
