747b93d32bf2a91ddb2cfbd1c6deeda75e04c444
AHNAF14924 <ahnaftahmid14924@gmail.com>
2023-07-28 00:53:09 +0600

    Added code

--- Web_Scraper/app.py
+++ Web_Scraper/app.py
@@ -8,7 +8,7 @@ sheet.title = "price"
 sheet.append(['Brand', 'Name', 'Price'])
 try:
     url = requests.get(
-        'https://www.techlandbd.com/pc-components/graphics-card?limit=100')
+        'https://www.techlandbd.com/pc-components/graphics-card?sort=p.price&order=ASC&fq=1&limit=100')
     url.raise_for_status()
     soup = BeautifulSoup(url.text, 'html.parser')
     cards = soup.find(
@@ -18,7 +18,7 @@ try:
         title = name.split(' ', 1)[1]
         brand = name.split(' ', 1)[0]
         price = card.find('div', class_='price').span.text
-        print(title, price)
+        print(brand, title, price)
         sheet.append([brand, title, price])
 except Exception as e:
     print(e)
